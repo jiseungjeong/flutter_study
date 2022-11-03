946bc28a6a69b9f1360997d4dbc2af88e836bacc
jiseungjeong <wjdwltmd1151@gmail.com>
2022-11-03 21:59:13 +0900

    learn with coding chef flutter mild video until 16th lecture

--- app_icon_menu/lib/main.dart
+++ app_icon_menu/lib/main.dart
@@ -52,6 +52,16 @@ class MyPage extends StatelessWidget {
                 backgroundImage: AssetImage('assets/user_image.png'),
                 backgroundColor: Colors.white,
               ),
+              otherAccountsPictures: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/user2_image.png'),
+                  backgroundColor: Colors.white,
+                ),
+                // CircleAvatar(
+                //   backgroundImage: AssetImage('assets/user2_image.png'),
+                //   backgroundColor: Colors.white,
+                // ),
+              ],
               accountName: Text('USER'),
               accountEmail: Text('user@user.com'),
               onDetailsPressed: () {
@@ -63,6 +73,39 @@ class MyPage extends StatelessWidget {
                     bottomLeft: Radius.circular(40.0),
                     bottomRight: Radius.circular(40.0),
                   )),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.grey[850],
+              ),
+              title: Text('Home'),
+              onTap: () {
+                print('Home is clicked.');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.grey[850],
+              ),
+              title: Text('Setting'),
+              onTap: () {
+                print('Setting is clicked.');
+              },
+              trailing: Icon(Icons.add),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.question_answer,
+                color: Colors.grey[850],
+              ),
+              title: Text('Q&A'),
+              onTap: () {
+                print('Q&A is clicked.');
+              },
+              trailing: Icon(Icons.add),
             ),
           ],
         ),
